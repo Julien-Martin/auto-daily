09a316d75e3d81e95b248fc098f18ff3022f9705
NitramJulien <julienmartin@codeassist.io>
2020-11-07 14:14:02 +0100

    Refactoring

--- index.js
+++ index.js
@@ -11,38 +11,29 @@ clear();
 
 console.log(
     chalk.yellow(
-        figlet.textSync('AutoDaily', { horizontalLayout: 'full'})
+        figlet.textSync('AutoDaily', {horizontalLayout: 'full'})
     )
 );
 
 const run = async (sendDaily = false) => {
-  const tasks = await Freebe.getTodayTasks();
-  if (!tasks.length) {
-    console.log(chalk.red('No tasks today.'))
+    const tasks = await Freebe.getTodayTasks();
+    if (!tasks.length) {
+        console.log(chalk.red('No tasks today.'))
+        process.exit(0);
+    }
+    let tomorrowTask = await inquirer.askTomorrowTask();
+    tomorrowTask = Enyo.createTomorrowTask(tomorrowTask.response);
+    const projectsList = await Enyo.getProjects();
+    const selectedProject = await inquirer.askProject(projectsList);
+    const dailyStandUp = await Enyo.createDailyStandUp(tasks, tomorrowTask, selectedProject);
+    if (sendDaily) {
+        const dailyStandUpSended = await Enyo.sendDailyStandUp(dailyStandUp);
+        console.log('Daily Send');
+        console.log(`${process.env.ENYO_APP}/daily_standup/${dailyStandUpSended._id}`);
+    } else {
+        console.log(dailyStandUp);
+    }
     process.exit(0);
-  }
-  let tomorrowTask = await inquirer.askTomorrowTask();
-  tomorrowTask = Enyo.createTomorrowTask(tomorrowTask.response);
-  const projectsList = await Enyo.getProjects();
-  const selectedProject = await inquirer.askProject(projectsList);
-  const dailyStandUp = await Enyo.createDailyStandUp(tasks, tomorrowTask, selectedProject);
-  if (sendDaily) {
-    Enyo.sendDailyStandUp(dailyStandUp)
-        .then((response) => {
-          if (response.data) {
-            console.log('Daily Send');
-            console.log(`${process.env.ENYO_APP}/daily_standup/${response.data.body._id}`);
-          }
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-        .finally(() => {
-          process.exit(0);
-        })
-  } else {
-    console.log(dailyStandUp);
-  }
 }
 
 run(false);
